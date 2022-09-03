e74727bf6708fe8299416ea7696896231d9cd414
bertalan83 <e.bertalan96@gmail.com>
2022-09-03 11:36:48 +0200

    task 5 done -> converting officer class to userdetails

--- src/main/java/app/services/OfficerService.java
+++ src/main/java/app/services/OfficerService.java
@@ -1,7 +1,22 @@
 package app.services;
 
+import app.repositories.OfficerRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OfficerService {
+public class OfficerService implements UserDetailsService {
+
+    private final OfficerRepo officerRepo;
+
+    public OfficerService(OfficerRepo officerRepo) {
+        this.officerRepo = officerRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return officerRepo.findByUsername(username);
+    }
 }
