ed2141c8b671336cade93624ca97a18c44719e0f
bertalan83 <e.bertalan96@gmail.com>
2022-09-03 12:55:25 +0200

    task 6 PARTLY done -> 1. when trying to register with duplicate username whitelabel error 2. -> login doesnt work keep saying bad creditentials ???? apart from this login details are saved in db

--- src/main/java/app/controllers/HomeAndLoginController.java
+++ src/main/java/app/controllers/HomeAndLoginController.java
@@ -1,11 +1,20 @@
 package app.controllers;
 
+import app.models.Officer;
+import app.services.OfficerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeAndLoginController {
+
+    private final OfficerService officerService;
+
+    public HomeAndLoginController(OfficerService officerService) {
+        this.officerService = officerService;
+    }
 
     @GetMapping(value = {"/", "/home", "/fooldal"})
     public String getHomePage() {
@@ -24,4 +33,17 @@ public class HomeAndLoginController {
         return "login";
     }
 
+    @GetMapping(value = {"/register"})
+    public String saveUserPage(Model model) {
+        model.addAttribute("user", new Officer());
+
+        return "register";
+    }
+
+    @PostMapping(value = {"/register"})
+    public String saveUser(Officer user) {
+        officerService.saveUser(user);
+
+        return "redirect:/login";
+    }
 }
--- src/main/java/app/services/OfficerService.java
+++ src/main/java/app/services/OfficerService.java
@@ -1,22 +1,39 @@
 package app.services;
 
+import app.models.Officer;
 import app.repositories.OfficerRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @Service
 public class OfficerService implements UserDetailsService {
 
-    private final OfficerRepo officerRepo;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    public OfficerService(OfficerRepo officerRepo) {
-        this.officerRepo = officerRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    public OfficerService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return officerRepo.findByUsername(username);
+        return entityManager.createQuery("SELECT u FROM Officer u WHERE u.username = :name", Officer.class)
+                .setParameter("name", username)
+                .getSingleResult();
+    }
+
+    @Transactional
+    public void saveUser (Officer user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        entityManager.persist(user);
     }
 }
